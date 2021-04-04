a5fd5438e6fb33c7c836c690c3e3ca051519bc6a
bvegaM <vegabryam40@gmail.com>
2021-04-03 19:34:45 -0500

    change in app.py

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ from flask import jsonify
 
 app = Flask(__name__)
 
+@app.route('/')
+def homepage():
+    return """
+    <h1>Machine Learning Model</h1>
+    <p>Bryam David Vega Moreno</p>
+    """
+
 @app.route('/predict',methods=['GET'])
 def predict():
     X_test = np.array([7.594444821,7.479555538,1.616463184,1.53352356,0.796666503,0.635422587,0.362012237,0.315963835,2.277026653])
